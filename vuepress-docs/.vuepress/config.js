6f1439f5f2aa6e42a1e43048f91b900c8c35ab60
R22627 <lvlv R22627/autelrobotics>
2023-05-23 17:56:31 +0800

    feat:修改导航栏

--- vuepress-docs/.vuepress/config.js
+++ vuepress-docs/.vuepress/config.js
@@ -34,16 +34,15 @@ module.exports = {
     logo: "https://excalidraw.com/apple-touch-icon.png",
     nav: [
       { text: '首页', link: '/' },
-      { text: '关于', link: 'https://u1s1.vip/about' },
-      { text: '友链', link: 'https://u1s1.vip' },
       {
         text: '下拉菜单',
         ariaLabel: '这是提示语',
         items: [
-          { text: '下拉1', link: '/', target:'_self' },
-          { text: '下拉2', link: 'https://u1s1.vip', target:'_blank' }
+          { text: '首页', link: '/', target:'_self' },
+          { text: 'TODO', link: '', target:'_blank' }
         ]
-      }
+      },
+      { text: 'Github', link: 'https://github.com/dablelv/backend-cmd'}
     ],
     sidebarDepth:0,
     sidebar:[
